060841ff22f0f0a59056ca1830bc7dc3a6c6d4e1
Guillaume Roux <roux.gensou@gmail.com>
2016-11-14 17:37:37 +0100

    Modified Bootstrap Grid For Inscription Form

--- inscription/index.php
+++ inscription/index.php
@@ -19,20 +19,24 @@ Formulaire d'inscription <br />
 pour la formation du<?php echo strftime("%d/%m/%y",strtotime($formation->date_debut)) ." au ". strftime("%d/%m/%y",strtotime($formation->date_fin)) ; ?>
 </small>
 </h1>
-<p id="result"></p>
+<br />
+<p>
+<span style="color:#B40404;">Important</span>
+Les Fichiers à transmettre doivent impérativement être au format pdf et avoir une taille inférieure à 3Mo
+</p>
 <br />
 </div>
 <div class="row">
-<fieldset class="form-group col-md-4 ">
+<fieldset class="form-group ">
 <label for="date_naissance">Date de Naissance *</label>
 <input type="date"  name="date_naissance" id="date_naissance" class="form-control" required />
 <input type="hidden" name="age" id="age" />
 </fieldset>
-<fieldset class="form-group col-md-4 ">
+<fieldset class="form-group ">
 <label for="lieu_naissance">Lieu de Naissance *</label>
 <input type="text"  name="lieu_naissance" id="lieu_naissance" class="form-control" required />
 </fieldset>
-<fieldset class="form-group col-md-4 ">
+<fieldset class="form-group ">
 <label for="paiement">Moyen de Paiement *</label>
 <select name="paiement" id="paiement" required style="margin:0;height: auto;">
 <option selected value=""></option>
@@ -43,97 +47,128 @@ pour la formation du<?php echo strftime("%d/%m/%y",strtotime($formation->date_de
 </div>
 <div class="row">
 <h2>Diplôme éventuels</h2><br />
-<fieldset class="form-group col-md-3 "style="overflow:hidden;">
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group  ">
 <label for="diplome">Diplôme </label>
 <input type="text"  name="diplome" id="diplome" class="form-control"  />
 </fieldset>
-<fieldset class="form-group col-md-3 "style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group ">
 <label for="no_diplome">N° de Diplôme </label>
 <input type="text"  name="no_diplome" id="no_diplome" class="form-control"  />
 </fieldset>
-<fieldset class="form-group col-md-3 "style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group ">
 <label for="date_diplome">Date de diplome </label>
 <input type="date"  name="date_diplome" id="date_diplome" class="form-control" />
 </fieldset>
-<fieldset class="form-group col-md-3 "style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group ">
 <label for="dr_delivrance">DR de délivrance</label>
 <input type="text"  name="dr_delivrance" id="dr_delivrance" class="form-control"  />
 </fieldset>
 </div>
+</div>
 <div class="row">
 <h2>Documents administratifs obligatoires</h2><br />
-<fieldset class="form-group col-md-3 obligatoire "style="overflow:hidden;">
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group obligatoire ">
 <p style="line-height:25px;">Carte d'identité (recto verso) ou passeport *</p>
 <div class="input">
 <input type="file" name="carte" id="carte" required />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-3 obligatoire "style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group obligatoire ">
 <p>Certificat Médical *</p>
 <div class="input">
 <input type="file" name="certif" id="certif" required />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-3 obligatoire"style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group obligatoire">
 <p style="line-height:25px;" >Justificatif d'assurance maladie *</p>
 <div class="input">
 <input type="file" name="assurance" id="assurance" required />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-3 obligatoire"style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group obligatoire">
 <p>Attestation de recensement *</p>
 <div class="input">
 <input type="file" name="defense" id="defense" required />
 </div>
 </fieldset>
 </div>
+</div>
 <h2>Documents complémentaires</h2><br />
 <div class="row">
-<fieldset class="form-group col-md-3 options "style="overflow:hidden;">
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group options ">
 <p>document 1</p>
 <div class="input">
 <input type="file" name="doc1" id="doc1"  />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-3 options "style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group options ">
 <p>document 2</p>
 <div class="input">
 <input type="file" name="doc2" id="doc2"  />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-3 options"style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group options">
 <p>document 3</p>
 <div class="input">
 <input type="file" name="doc3" id="doc3"  />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-3 options"style="overflow:hidden;">
+</div>
+<div class="col-md-3" style="overflow:hidden">
+<fieldset class="form-group options">
 <p>document 4</p>
 <div class="input">
 <input type="file" name="doc4" id="doc4"  />
 </div>
 </fieldset>
 </div>
+</div>
 <div class="row">
-<fieldset class="form-group col-md-4 options"style="overflow:hidden;">
+<div class="col-md-4" style="overflow:hidden">
+<fieldset class="form-group options">
 <p>document 5</p>
 <div class="input">
 <input type="file" name="doc5" id="doc5"  />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-4 options"style="overflow:hidden;">
+</div>
+<div class="col-md-4" style="overflow:hidden">
+<fieldset class="form-group options">
 <p>document 6</p>
 <div class="input">
 <input type="file" name="doc6" id="doc6"  />
 </div>
 </fieldset>
-<fieldset class="form-group col-md-4 options"style="overflow:hidden;">
+</div>
+<div class="col-md-4" style="overflow:hidden">
+<fieldset class="form-group options">
 <p>document 7</p>
 <div class="input">
 <input type="file" name="doc7" id="doc7"  />
 </div>
 </fieldset>
 </div>
+</div>
+<p id="result"></p>
 <input type="submit" name="inscrire" id="inscrire" value="Transmettre" />
 </form>
 <?php
